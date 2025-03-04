74ed7ac54c08ab6a37012559bac8dd1cc639c6aa
day2906 <angel.dayan.mtz@gmail.com>
2025-03-03 19:14:25 -0600

    Update contacto.js

--- contacto.js
+++ contacto.js
@@ -1,6 +1,6 @@
 function enviarMensaje() {
     let numero = "5215580078471"; // Reemplaza con tu número
-    let mensaje = encodeURIComponent("Hola, quiero más información");
+    let mensaje = encodeURIComponent("Hola, mi nombre es [NOMBRE], quiero confirmar mi asistencia a la boda de Daniel y Naomi. Iremos [NUMERO] personas.");
     let url = `https://wa.me/${numero}?text=${mensaje}`;
     window.open(url, "_blank");
 }
